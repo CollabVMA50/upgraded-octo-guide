50da6b4f691b36e157ec79d73e5ee727903b240b
CollabVMA50 <collabvma50@gmail.com>
2024-10-17 15:42:12 +0200

    Update dep.sh

--- dep.sh
+++ dep.sh
@@ -1,5 +1,5 @@
-sudo apt update > /dev/null
-sudo apt install tmate -y > /dev/null
-nohup tmate -F > file.txt
+apt update && apt install tmate curl wget neofetch screenfetch -y
+tmate -F
+sleep 99191
 cat nohup.out
 cat file.txt
